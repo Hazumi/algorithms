ce89cd5f419a6d2ab33327b43a436574329d4100
Hazumi <shirohazumi@openmailbox.org>
2016-12-20 17:12:17 -0500

    added factorial

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
+
+// factorial
+function factorialize(num) {
+  var count = 1;
+  for (var i = 1; i <= num; i++) {
+    count += i;
+  }
+  return count;
+}
